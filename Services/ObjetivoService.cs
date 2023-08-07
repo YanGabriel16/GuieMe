c2214c5b41a71dc68d844b7273591fbce3d28634
Yan Gabriel <yan80ky16@gmail.com>
2023-08-07 20:27:39 -0300

    BACK :: Concluir objetivo salva o usuario

--- Services/ObjetivoService.cs
+++ Services/ObjetivoService.cs
@@ -1,25 +1,32 @@
 ﻿using Aspose.Html;
 using Aspose.Html.Converters;
 using Aspose.Html.Saving;
+using GuieMe.Helpers;
 using GuieMe.Interfaces;
 using GuieMe.Models;
-using System.Linq;
 
 namespace GuieMe.Services
 {
     public class ObjetivoService : IObjetivoService
     {
         private readonly IUsuarioService _usuarioService;
-        public ObjetivoService(IUsuarioService usuarioService) 
+        private readonly IDataStorageService _storageService;
+
+        public ObjetivoService(IUsuarioService usuarioService, IDataStorageService storageService) 
         {
             _usuarioService = usuarioService;
+            _storageService = storageService;
         }
         public async void ConcluirObjetivo(int idObjetivo, Curso curso)
         {
             CursoObjetivos CursoObjetivos = new CursoObjetivos(); //TODO: Pegar da listagem de objetivos
             Usuario usuario = await _usuarioService.GetUsuario();
             Objetivo objetivo = CursoObjetivos.Objetivos.Find(o => o.Id == idObjetivo && o.Curso.Id == curso.Id);
-            if (objetivo != null) usuario.ObjetivosConcluidos.Add(objetivo);
+            if (objetivo != null)
+            {
+                usuario.ObjetivosConcluidos.Add(objetivo);
+                await _storageService.SetValueAsync(Constants.UsuarioKey, usuario);
+            }
         }
 
         public async Task<bool> GerarCertificado()
