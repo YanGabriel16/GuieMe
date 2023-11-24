ea14b9a8ab9ee92299b4a57d3e9ea07c0d93eec8
Yan Gabriel Silva Queiroz <yan80ky16-gsq@hotmail.com>
2023-11-24 11:27:42 -0300

    Definindo locais do mapa

--- Infra/Services/LocalService.cs
+++ Infra/Services/LocalService.cs
@@ -11,9 +11,8 @@ namespace GuieMe.Infra.Services
         {
             List<Andar> andares = new()
             {
-                new Andar(0, "mapaTerreo") { Pisoo = 1, IndexAndar = 0 },
-                new Andar(1, "mapaPrimeiroAndar") { Pisoo = 2, IndexAndar = 1 },
-                new Andar(2, "mapaSegundoAndar") { Pisoo = 3, IndexAndar = 2 }
+                new Andar(0, "mapaSubSolo") { Pisoo = 1, IndexAndar = 0 },
+                new Andar(1, "mapaTerreo")  { Pisoo = 2, IndexAndar = 1 },
             };
 
             return andares.OrderBy(o => o.Nome).ToList();
@@ -23,26 +22,77 @@ namespace GuieMe.Infra.Services
         {
             List<Local> locais = new()
             {
-              //  new Local(0, "Predio Principal Entrada") { LatitudeMin = 00, LongitudeMin = 00, LatitudeMax = 00, LongitudeMax = 00, LatitudeEntrada = 00, LongitudeEntrada = 00, Pisoo = 1, AndarId = 0},
-                new Local(1, "Secretaria Veteranos")     { LatitudeMin = 03, LongitudeMin = 01, LatitudeMax = 09, LongitudeMax = 05, LatitudeEntrada = 05, LongitudeEntrada = 05, Pisoo = 1, AndarId = 0},
-                new Local(2, "Papelaria")                { LatitudeMin = 09, LongitudeMin = 01, LatitudeMax = 17, LongitudeMax = 05, LatitudeEntrada = 12, LongitudeEntrada = 05, Pisoo = 1, AndarId = 0},
-                new Local(3, "Cafeteria Expresso")       { LatitudeMin = 17, LongitudeMin = 01, LatitudeMax = 28, LongitudeMax = 04, LatitudeEntrada = 26, LongitudeEntrada = 04, Pisoo = 1, AndarId = 0},
-                new Local(4, "Secretaria Calouros")      { LatitudeMin = 01, LongitudeMin = 09, LatitudeMax = 07, LongitudeMax = 13, LatitudeEntrada = 07, LongitudeEntrada = 10, Pisoo = 1, AndarId = 0},
-                new Local(5, "Coordenação")              { LatitudeMin = 14, LongitudeMin = 08, LatitudeMax = 19, LongitudeMax = 13, LatitudeEntrada = 14, LongitudeEntrada = 10, Pisoo = 1, AndarId = 0},
-                new Local(6, "Laboratório")              { LatitudeMin = 19, LongitudeMin = 09, LatitudeMax = 27, LongitudeMax = 13, LatitudeEntrada = 27, LongitudeEntrada = 11, Pisoo = 1, AndarId = 0},
+                //  new Local(0, "Predio Principal Entrada") { LatitudeMin = 00, LongitudeMin = 00, LatitudeMax = 00, LongitudeMax = 00, LatitudeEntrada = 00, LongitudeEntrada = 00, Pisoo = 1, AndarId = 0},
+                //new Local(1, "Secretaria Veteranos")     { LatitudeMin = 03, LongitudeMin = 01, LatitudeMax = 09, LongitudeMax = 05, LatitudeEntrada = 05, LongitudeEntrada = 05, Pisoo = 1, AndarId = 0},
+                //new Local(2, "Papelaria")                { LatitudeMin = 09, LongitudeMin = 01, LatitudeMax = 17, LongitudeMax = 05, LatitudeEntrada = 12, LongitudeEntrada = 05, Pisoo = 1, AndarId = 0},
+                //new Local(3, "Cafeteria Expresso")       { LatitudeMin = 17, LongitudeMin = 01, LatitudeMax = 28, LongitudeMax = 04, LatitudeEntrada = 26, LongitudeEntrada = 04, Pisoo = 1, AndarId = 0},
+                //new Local(4, "Secretaria Calouros")      { LatitudeMin = 01, LongitudeMin = 09, LatitudeMax = 07, LongitudeMax = 13, LatitudeEntrada = 07, LongitudeEntrada = 10, Pisoo = 1, AndarId = 0},
+                //new Local(5, "Coordenação")              { LatitudeMin = 14, LongitudeMin = 08, LatitudeMax = 19, LongitudeMax = 13, LatitudeEntrada = 14, LongitudeEntrada = 10, Pisoo = 1, AndarId = 0},
+                //new Local(6, "Laboratório")              { LatitudeMin = 19, LongitudeMin = 09, LatitudeMax = 27, LongitudeMax = 13, LatitudeEntrada = 27, LongitudeEntrada = 11, Pisoo = 1, AndarId = 0},
 
-                new Local(9, "Secretaria 2")             { LatitudeMin = 01, LongitudeMin = 01, LatitudeMax = 06, LongitudeMax = 10, LatitudeEntrada = 06, LongitudeEntrada = 08, Pisoo = 2, AndarId = 1},
-                new Local(10, "Papelaria 2")             { LatitudeMin = 09, LongitudeMin = 01, LatitudeMax = 17, LongitudeMax = 06, LatitudeEntrada = 16, LongitudeEntrada = 06, Pisoo = 2, AndarId = 1},
-                new Local(11, "Cafeteria 2")             { LatitudeMin = 22, LongitudeMin = 01, LatitudeMax = 28, LongitudeMax = 10, LatitudeEntrada = 22, LongitudeEntrada = 03, Pisoo = 2, AndarId = 1},
-                new Local(13, "Laboratório 2")           { LatitudeMin = 04, LongitudeMin = 10, LatitudeMax = 12, LongitudeMax = 14, LatitudeEntrada = 10, LongitudeEntrada = 10, Pisoo = 2, AndarId = 1},
-                new Local(14, "Coordenação 2")           { LatitudeMin = 16, LongitudeMin = 10, LatitudeMax = 24, LongitudeMax = 14, LatitudeEntrada = 16, LongitudeEntrada = 12, Pisoo = 2, AndarId = 1},
+                //new Local(9, "Secretaria 2")             { LatitudeMin = 01, LongitudeMin = 01, LatitudeMax = 06, LongitudeMax = 10, LatitudeEntrada = 06, LongitudeEntrada = 08, Pisoo = 2, AndarId = 1},
+                //new Local(10, "Papelaria 2")             { LatitudeMin = 09, LongitudeMin = 01, LatitudeMax = 17, LongitudeMax = 06, LatitudeEntrada = 16, LongitudeEntrada = 06, Pisoo = 2, AndarId = 1},
+                //new Local(11, "Cafeteria 2")             { LatitudeMin = 22, LongitudeMin = 01, LatitudeMax = 28, LongitudeMax = 10, LatitudeEntrada = 22, LongitudeEntrada = 03, Pisoo = 2, AndarId = 1},
+                //new Local(13, "Laboratório 2")           { LatitudeMin = 04, LongitudeMin = 10, LatitudeMax = 12, LongitudeMax = 14, LatitudeEntrada = 10, LongitudeEntrada = 10, Pisoo = 2, AndarId = 1},
+                //new Local(14, "Coordenação 2")           { LatitudeMin = 16, LongitudeMin = 10, LatitudeMax = 24, LongitudeMax = 14, LatitudeEntrada = 16, LongitudeEntrada = 12, Pisoo = 2, AndarId = 1},
 
-                new Local(7, "Escada 1")                 { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 08, LongitudeEntrada = 06, Pisoo = 1, Escada = true}, 
-                new Local(8, "Escada 2")                 { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 21, LongitudeEntrada = 08, Pisoo = 1, Escada = true},
-                new Local(15, "Escada 3")                 { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 08, LongitudeEntrada = 06, Pisoo = 2, Escada = true},
-                new Local(16, "Escada 4")                { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 21, LongitudeEntrada = 08, Pisoo = 2, Escada = true},
+                //new Local(7, "Escada 1")                 { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 08, LongitudeEntrada = 06, Pisoo = 1, Escada = true}, 
+                //new Local(8, "Escada 2")                 { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 21, LongitudeEntrada = 08, Pisoo = 1, Escada = true},
+                //new Local(15, "Escada 3")                { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 08, LongitudeEntrada = 06, Pisoo = 2, Escada = true},
+                //new Local(16, "Escada 4")                { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 21, LongitudeEntrada = 08, Pisoo = 2, Escada = true},
+
+
+                //Térreo ------------------------------------------------------------------
+                new Local(1, "Coordenação")                 { LatitudeMin = 00, LongitudeMin = 24, LatitudeMax = 05, LongitudeMax = 29, LatitudeEntrada = 04, LongitudeEntrada = 24, Pisoo = 2, AndarId = 1},
+                new Local(2, "Cafeteria 1")                 { LatitudeMin = 17, LongitudeMin = 00, LatitudeMax = 20, LongitudeMax = 04, LatitudeEntrada = 20, LongitudeEntrada = 03, Pisoo = 2, AndarId = 1},
+                new Local(3, "Pravaler")                    { LatitudeMin = 17, LongitudeMin = 07, LatitudeMax = 20, LongitudeMax = 11, LatitudeEntrada = 20, LongitudeEntrada = 09, Pisoo = 2, AndarId = 1},
+                new Local(4, "Pós Graduação")               { LatitudeMin = 17, LongitudeMin = 15, LatitudeMax = 20, LongitudeMax = 20, LatitudeEntrada = 20, LongitudeEntrada = 18, Pisoo = 2, AndarId = 1},
+                new Local(5, "Cafeteria 2")                 { LatitudeMin = 17, LongitudeMin = 24, LatitudeMax = 20, LongitudeMax = 29, LatitudeEntrada = 20, LongitudeEntrada = 26, Pisoo = 2, AndarId = 1},
+                new Local(6, "Papelaria")                   { LatitudeMin = 17, LongitudeMin = 34, LatitudeMax = 20, LongitudeMax = 39, LatitudeEntrada = 20, LongitudeEntrada = 37, Pisoo = 2, AndarId = 1},
+                new Local(7, "Banheiro Feminino")           { LatitudeMin = 13, LongitudeMin = 24, LatitudeMax = 15, LongitudeMax = 26, LatitudeEntrada = 14, LongitudeEntrada = 24, Pisoo = 2, AndarId = 1},
+                new Local(8, "Cafeteria 3")                 { LatitudeMin = 39, LongitudeMin = 00, LatitudeMax = 42, LongitudeMax = 04, LatitudeEntrada = 39, LongitudeEntrada = 03, Pisoo = 2, AndarId = 1},
+                new Local(9, "Cópia Cabana")                { LatitudeMin = 39, LongitudeMin = 07, LatitudeMax = 42, LongitudeMax = 11, LatitudeEntrada = 39, LongitudeEntrada = 09, Pisoo = 2, AndarId = 1},
+                new Local(10, "Vestibular")                 { LatitudeMin = 39, LongitudeMin = 15, LatitudeMax = 42, LongitudeMax = 20, LatitudeEntrada = 39, LongitudeEntrada = 18, Pisoo = 2, AndarId = 1},
+                new Local(11, "Cafeteria 4")                { LatitudeMin = 39, LongitudeMin = 24, LatitudeMax = 42, LongitudeMax = 29, LatitudeEntrada = 39, LongitudeEntrada = 27, Pisoo = 2, AndarId = 1},
+                new Local(12, "Banheiro Feminino")          { LatitudeMin = 44, LongitudeMin = 18, LatitudeMax = 46, LongitudeMax = 20, LatitudeEntrada = 45, LongitudeEntrada = 20, Pisoo = 2, AndarId = 1},
+                new Local(13, "Banheiro Masculino")         { LatitudeMin = 44, LongitudeMin = 24, LatitudeMax = 46, LongitudeMax = 26, LatitudeEntrada = 45, LongitudeEntrada = 24, Pisoo = 2, AndarId = 1},
+                new Local(14, "Computadores")               { LatitudeMin = 39, LongitudeMin = 34, LatitudeMax = 42, LongitudeMax = 39, LatitudeEntrada = 39, LongitudeEntrada = 37, Pisoo = 2, AndarId = 1},
+                new Local(25, "Banheiro Masculino")         { LatitudeMin = 13, LongitudeMin = 37, LatitudeMax = 15, LongitudeMax = 39, LatitudeEntrada = 14, LongitudeEntrada = 39, Pisoo = 2, AndarId = 1},
+                new Local(27, "Secretaria Veteranos")       { LatitudeMin = 48, LongitudeMin = 34, LatitudeMax = 54, LongitudeMax = 39, LatitudeEntrada = 50, LongitudeEntrada = 39, Pisoo = 2, AndarId = 1},
+                new Local(28, "Secretaria Calouros")        { LatitudeMin = 54, LongitudeMin = 34, LatitudeMax = 59, LongitudeMax = 39, LatitudeEntrada = 57, LongitudeEntrada = 39, Pisoo = 2, AndarId = 1},
+
+                //Escadas
+                new Local(15, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 17, LongitudeEntrada = 23, Pisoo = 1, Escada = true},
+                new Local(16, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 43, LongitudeEntrada = 21, Pisoo = 1, Escada = true},
+                new Local(19, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 16, LongitudeEntrada = 40, Pisoo = 1, Escada = true},
+
+                //quarteirões
+                new Local(356565, "") { LatitudeMin = 00, LongitudeMin = 00, LatitudeMax = 17, LongitudeMax = 11, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false },
+                new Local(356566, "") { LatitudeMin = 00, LongitudeMin = 24, LatitudeMax = 17, LongitudeMax = 39, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false },
+                new Local(356567, "") { LatitudeMin = 42, LongitudeMin = 24, LatitudeMax = 59, LongitudeMax = 39, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false },
+                new Local(356568, "") { LatitudeMin = 42, LongitudeMin = 00, LatitudeMax = 59, LongitudeMax = 11, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false },
+                new Local(356569, "") { LatitudeMin = 00, LongitudeMin = 15, LatitudeMax = 17, LongitudeMax = 20, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false},
+                new Local(356570, "") { LatitudeMin = 39, LongitudeMin = 15, LatitudeMax = 59, LongitudeMax = 20, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 2, AndarId = 1, LocalAcessivel = false },
+
+
+                //SubSolo ------------------------------------------------------------------
+                new Local(29, "Banheiro Masculino")       { LatitudeMin = 14, LongitudeMin = 21, LatitudeMax = 16, LongitudeMax = 23, LatitudeEntrada = 16, LongitudeEntrada = 22, Pisoo = 1, AndarId = 0},
+                new Local(30, "Pós Odontologia")          { LatitudeMin = 20, LongitudeMin = 21, LatitudeMax = 26, LongitudeMax = 26, LatitudeEntrada = 20, LongitudeEntrada = 23, Pisoo = 1, AndarId = 0},
+                new Local(31, "Laboratório Enfermagem")   { LatitudeMin = 20, LongitudeMin = 26, LatitudeMax = 26, LongitudeMax = 31, LatitudeEntrada = 20, LongitudeEntrada = 27, Pisoo = 1, AndarId = 0},
+                new Local(32, "Triagem Odontologia")      { LatitudeMin = 20, LongitudeMin = 31, LatitudeMax = 26, LongitudeMax = 36, LatitudeEntrada = 26, LongitudeEntrada = 35, Pisoo = 1, AndarId = 0},
+                new Local(33, "Copa")                     { LatitudeMin = 19, LongitudeMin = 41, LatitudeMax = 23, LongitudeMax = 45, LatitudeEntrada = 21, LongitudeEntrada = 41, Pisoo = 1, AndarId = 0},
+                new Local(33, "Refeitório")               { LatitudeMin = 23, LongitudeMin = 41, LatitudeMax = 27, LongitudeMax = 45, LatitudeEntrada = 25, LongitudeEntrada = 41, Pisoo = 1, AndarId = 0},
+                new Local(34, "Banheiro Feminino")        { LatitudeMin = 14, LongitudeMin = 34, LatitudeMax = 16, LongitudeMax = 36, LatitudeEntrada = 16, LongitudeEntrada = 35, Pisoo = 1, AndarId = 0},
+
+                //Escadas
+                new Local(20, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 17, LongitudeEntrada = 23, Pisoo = 2, Escada = true},
+                new Local(21, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 43, LongitudeEntrada = 21, Pisoo = 2, Escada = true},
+                new Local(24, "Escada")                    { LatitudeMin = -1, LongitudeMin = -1, LatitudeMax = -1, LongitudeMax = -1, LatitudeEntrada = 16, LongitudeEntrada = 40, Pisoo = 2, Escada = true},
+
+                ////quarteirões
+                new Local(356571, "") { LatitudeMin = 00, LongitudeMin = 21, LatitudeMax = 16, LongitudeMax = 36, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 1, AndarId = 0, LocalAcessivel = false },
+                new Local(356572, "") { LatitudeMin = 00, LongitudeMin = 41, LatitudeMax = 59, LongitudeMax = 45, LatitudeEntrada = -1, LongitudeEntrada = -1, Pisoo = 1, AndarId = 0, LocalAcessivel = false },
             };
-
             return locais.OrderBy(o => o.Nome).ToList();
         }
     }
--- Infra/Services/ObjetivoService.cs
+++ Infra/Services/ObjetivoService.cs
@@ -66,10 +66,10 @@ namespace GuieMe.Infra.Services
             var objetivos = new List<Objetivo>()
             {
               //  new Objetivo(0, 0, null, "Inicie reconhecendo o predio principal"),
-                new Objetivo(1, 4, null, "Uma boa vida academica vem da facilidade de resolver problemas, e a secretaria dos calouros pode resolver alguns (se você for um calouro(a))"),
-                new Objetivo(2, 1, null, "Uma boa vida academica vem da facilidade de resolver problemas, e a secretaria dos veteranos pode resolver alguns (se você for um veterano(a))"),
-                new Objetivo(3, 5, null, "Nem todos os problemas podemos resolver nas secretarias, as vezes você pode achar a solução aqui"),
-                new Objetivo(4, 2, null, "As vezes é necessário uma lembrancinha academica ou até materiais academicos..."),
+                new Objetivo(1, 1, null, "Uma boa vida academica vem da facilidade de resolver problemas, e a secretaria dos calouros pode resolver alguns (se você for um calouro(a))"),
+                //new Objetivo(2, 1, null, "Uma boa vida academica vem da facilidade de resolver problemas, e a secretaria dos veteranos pode resolver alguns (se você for um veterano(a))"),
+                //new Objetivo(3, 5, null, "Nem todos os problemas podemos resolver nas secretarias, as vezes você pode achar a solução aqui"),
+                //new Objetivo(4, 2, null, "As vezes é necessário uma lembrancinha academica ou até materiais academicos..."),
             };
 
             var locais = _localService.Locais();
